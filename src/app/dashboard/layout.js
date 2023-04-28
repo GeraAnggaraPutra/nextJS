674bd840b5b8df3bc8cf3a02ed4bb7ea5628e583
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-28 21:00:40 +0700

    layout

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,8 @@
+export default function layout({ children }) {
+  return (
+    <div>
+      ini layout children
+      {children}
+    </div>
+  );
+}
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Dashboard",
+  description: "Dashboard page",
+};
+
+export default function Dashboard() {
+  return <div>Dashboard</div>;
+}
